5ad3bf747b3186e9b3da284169ba8576937844ed
Tochukwu Nwosa <tochukwunwosa28@gmail.com>
2025-06-29 23:41:50 +0100

    feat: Update Calendar component with new props and styling; add Tiptap dependencies

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,13 +1,13 @@
 import { forwardRef, useEffect, useImperativeHandle } from "react"
 import { useEditor, EditorContent } from "@tiptap/react"
-import StarterKit from "@tiptap/starter-kit"
-import Heading from "@tiptap/extension-heading"
-import Bold from "@tiptap/extension-bold"
-import Italic from "@tiptap/extension-italic"
-import Underline from "@tiptap/extension-underline"
-import BulletList from "@tiptap/extension-bullet-list"
-import OrderedList from "@tiptap/extension-ordered-list"
-import ListItem from "@tiptap/extension-list-item"
+// import StarterKit from "@tiptap/starter-kit"
+// import Heading from "@tiptap/extension-heading"
+// import Bold from "@tiptap/extension-bold"
+// import Italic from "@tiptap/extension-italic"
+// import Underline from "@tiptap/extension-underline"
+// import BulletList from "@tiptap/extension-bullet-list"
+// import OrderedList from "@tiptap/extension-ordered-list"
+// import ListItem from "@tiptap/extension-list-item"
 
 interface EditorProps {
   initialContent: string
@@ -20,23 +20,23 @@ interface EditorHandle {
   setContent: (content: string) => void
 }
 
-const Editor = forwardRef<EditorHandle, EditorProps>(({ initialContent, readOnly = false, onChange }, ref) => {
+const Editor = forwardRef<EditorHandle, EditorProps>(({ initialContent, readOnly = false }, ref) => {
   const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Heading,
-      Bold,
-      Italic,
-      Underline,
-      BulletList,
-      OrderedList,
-      ListItem,
-    ],
+    // extensions: [
+    //   StarterKit,
+    //   Heading,
+    //   Bold,
+    //   Italic,
+    //   Underline,
+    //   BulletList,
+    //   OrderedList,
+    //   ListItem,
+    // ],
     content: initialContent,
     editable: !readOnly,
-    onUpdate: ({ editor }) => {
-      onChange?.(editor.getHTML())
-    },
+    // onUpdate: ({ editor }) => {
+    //   onChange?.(editor.getHTML())
+    // },
   })
 
   useImperativeHandle(ref, () => ({
